3f8e768b5627ff49a89a93843e9301128fcd6d79
SESA701219 <SESA701219@WB-PF42JDEV>
2024-11-14 12:59:04 +0530

    Add project files.

---
+++ Domain/Images.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Images
+    {
+        public int Id { get; set; }
+        public int PropertyId { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public bool IsUploaded { get; set; }
+
+        public Property Property { get; set; }
+    }
+}
